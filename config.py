251f697a0f438b199d020412a55932ea2c778a8b
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-04-27 14:38:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27498866"
 api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
 bot_token = "7951210945:AAHLc6I83cGgusWzhtEz1T16UCK3wUVdk5Q"
-auth_users = [7890781002,6373737837]
+auth_users = [8080218275,7417246937]
