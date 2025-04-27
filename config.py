aea0a970121ab378b2263e82974f03bfd1d50b73
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-04-27 13:50:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "27498866"
+api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
+bot_token = "7951210945:AAHLc6I83cGgusWzhtEz1T16UCK3wUVdk5Q"
 auth_users = [7890781002,6373737837]
